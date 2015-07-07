013bd5915a33f8db44695b6d74a2bb6e435bb492
nfernal <nfernal13@outlook.com>
2015-07-06 20:30:48 -0500

    initial commit of chef cookbook files

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'war_server'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures war_server'
+long_description 'Installs/Configures war_server'
+
+version          '0.1.0'
+
